7bcf2c6c1ab9817503a49a66cd5999639947e0c0
jimiller20 <jmiller20twenty@gmail.com>
2024-01-14 22:14:32 -0600

    added react

---
+++ change-backend/change/src/main/groovy/com/cts/change/ChangeApplication.groovy
@@ -0,0 +1,13 @@
+package com.cts.change
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ChangeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ChangeApplication, args)
+	}
+
+}
---
+++ change-backend/change/src/test/groovy/com/cts/change/ChangeApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.cts.change
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
